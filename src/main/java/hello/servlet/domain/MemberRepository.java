4120f05056a118674649b7704a6dbf2fc95b4eeb
pebblepark <subling02@gmail.com>
2022-01-13 23:26:03 +0900

    회원 Respository 추가

---
+++ src/main/java/hello/servlet/domain/MemberRepository.java
@@ -0,0 +1,38 @@
+package hello.servlet.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    private static final MemberRepository instance = new MemberRepository();
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    // 싱글톤 패턴 적용을 위해 생성자 private으로 막아놓기
+    private MemberRepository() { }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
