9144c0e0fdbeb3b1f64f3cf08627ac14bf1c1984
pebblepark <subling02@gmail.com>
2022-01-18 20:42:33 +0900

    핸들러 매핑, 핸들러 어댑터, 뷰 리졸버 예제

---
+++ src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.springmvc.old;
+
+import hello.servlet.domain.member.Member;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+        response.addHeader("test", "1234");
+    }
+
+}
